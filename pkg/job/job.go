f44cadb24944a71bbe618cd3df3bf15ee99efeed
Jose del Río <josedelrio85@gmail.com>
2023-09-18 23:09:08 +0200

    job implementation

--- pkg/job/job.go
+++ pkg/job/job.go
@@ -1,6 +1,12 @@
 package job
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/josedelrio85/test_cache/pkg"
+)
 
 type Job interface {
 	Run()
@@ -14,4 +20,29 @@ func RegisterJob(j Job) {
 			time.Sleep(j.SleepTime())
 		}
 	}(j)
+}
+
+type myJob struct {
+	cacheClient *memcache.Client
+	safeCounter pkg.SafeCounter
+}
+
+func NewJob(cacheClient *memcache.Client, counter pkg.SafeCounter) *myJob {
+	return &myJob{
+		cacheClient: cacheClient,
+		safeCounter: counter,
+	}
+}
+
+func (m myJob) Run() {
+	m.safeCounter.Inc("counter")
+	counterStr := fmt.Sprintf("%d", m.safeCounter.Value("counter"))
+	fmt.Println("Running...", counterStr)
+	if err := m.cacheClient.Set(&memcache.Item{Key: "counter", Value: []byte(counterStr)}); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (myJob) SleepTime() time.Duration {
+	return time.Second * 5
 }
